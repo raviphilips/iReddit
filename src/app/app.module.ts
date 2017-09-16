ba3e3c9f2e9fc6881e3f5c195432a943e9e28ab6
raviphilips <rakandula@hotmail.com>
2017-09-16 22:53:54 +0530

    First Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,15 @@
 import { NgModule, ErrorHandler } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
+import { HttpModule} from '@angular/http';
 import { MyApp } from './app.component';
 
 import { AboutPage } from '../pages/about/about';
 import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
+import { RedditPage } from '../pages/reddit/reddit';
+import { SettingsPage } from '../pages/settings/settings';
+import { DetailsPage } from '../pages/details/details';
 import { TabsPage } from '../pages/tabs/tabs';
 
 import { StatusBar } from '@ionic-native/status-bar';
@@ -17,11 +21,15 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     AboutPage,
     ContactPage,
     HomePage,
+    RedditPage,
+    SettingsPage,
+    DetailsPage,
     TabsPage
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    HttpModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
@@ -29,6 +37,9 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     AboutPage,
     ContactPage,
     HomePage,
+    RedditPage,
+    SettingsPage,
+    DetailsPage,
     TabsPage
   ],
   providers: [
---
+++ src/pages/settings/settings.ts
@@ -0,0 +1,38 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import { RedditService } from '../../app/services/reddit.service';
+import { RedditPage } from '../reddit/reddit'
+
+@Component({
+  selector: 'page-settings',
+  templateUrl: 'settings.html'
+})
+export class SettingsPage {
+
+  category: any;
+	limit: any;
+  constructor(public navCtrl: NavController, private redditService: RedditService) {
+  	this.getDefault();
+  }
+
+  getDefault(){
+  	if(localStorage.getItem('category') != null){
+  		this.category = localStorage.getItem('category');
+  	} else{
+  		this.category = 'sports';
+  	}
+
+  	if(localStorage.getItem('limit') != null){
+  		this.limit = localStorage.getItem('limit');
+  	} else{
+  		this.limit = '10';
+  	}
+  }
+ 
+ setDefaults(){
+ 	localStorage.setitem('category', this.category);
+ 	localStorage.setitem('limit', this.limit);
+ 	this.navCtrl.push(RedditPage);
+ }
+
+}
